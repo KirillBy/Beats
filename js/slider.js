e2f2fcdcc32ff57196ac7f8eb7a2d7b7eb56fecf
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-06 18:23:48 +0300

    modal done

--- js/slider.js
+++ js/slider.js
@@ -4,15 +4,7 @@ const sliderOptions = {
   }
   
   const slider = $(".products").bxSlider(sliderOptions);
-  jQuery.event.special.touchstart = {
-    setup: function( _, ns, handle ){
-      if ( ns.includes("noPreventDefault") ) {
-        this.addEventListener("touchstart", handle, { passive: false });
-      } else {
-        this.addEventListener("touchstart", handle, { passive: true });
-      }
-    }
-  };
+
   $('.products-slider__btn--next').click(e => {
     e.preventDefault();
     slider.goToNextSlide();
